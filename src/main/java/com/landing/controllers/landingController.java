12be26ac22a91cd24ffc766742e44bd35c605885
Andres Arango <pipe1411@gmail.com>
2017-09-11 01:07:29 -0400

    Initial commit

---
+++ src/main/java/com/landing/controllers/landingController.java
@@ -0,0 +1,31 @@
+/*
+package com.landing.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+*/
+/**
+ * Created by pipe on 9/11/17.
+ *//*
+
+
+@RestController
+public class landingController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        System.out.println("index request");
+        return "index";
+    }
+
+    @RequestMapping("/landing")
+    public String landing(Model model) {
+        System.out.println("landing request");
+        return "landing";
+    }
+}
+
+*/
